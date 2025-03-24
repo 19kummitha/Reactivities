f068348a53b4d8962aa45a35daa9609a9f67b760
19kummitha <113335352+19kummitha@users.noreply.github.com>
2025-03-24 14:06:26 +0530

    mobx implemented

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,9 +9,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/Router/Routes";
+import { store, StoreContext } from "./lib/stores/store";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <StoreContext.Provider value={store} />
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
       <RouterProvider router={router} />
